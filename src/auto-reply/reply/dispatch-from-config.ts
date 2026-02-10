andysheldon-creator/OpenClaw-Optimised

feat(hooks): add config-driven message handlers for immediate agent triggering
Add `hooks.internal.messageHandlers` config array for triggering agent
execution when inbound messages match specified conditions. This solves
the problem of important messages (like bug reports) being queued and
potentially missed when cron jobs wake the agent.

Features:
- Match conditions: channelId, conversationId, from, contentPattern (regex),
  contentContains (keywords)
- Priority modes: "immediate" (bypass queue) or "queue" (normal flow)
- Execution modes: "exclusive" (handler only) or "parallel" (handler + normal)
- Message transformation: prefix, suffix, or full template with placeholders
- Configurable model and thinking level per handler

Implementation:
- src/config/types.hooks.ts: MessageHandlerConfig and MessageHandlerMatch types
- src/config/zod-schema.hooks.ts: Zod validation schemas
- src/hooks/message-handler-match.ts: Condition matching logic
- src/hooks/message-handler-run.ts: Handler executor using runCronIsolatedAgentTurn
- src/auto-reply/reply/dispatch-from-config.ts: Integration point
- docs/hooks.md: Full documentation with examples
- 28 unit tests for matcher logic

src/auto-reply/reply/dispatch-from-config.ts
@@ -1,8 +1,12 @@
-import type { MoltbotConfig } from "../../config/config.js";
 import { resolveSessionAgentId } from "../../agents/agent-scope.js";
+import { createDefaultDeps } from "../../cli/deps.js";
+import type { MoltbotConfig } from "../../config/config.js";
 import { loadSessionStore, resolveStorePath } from "../../config/sessions.js";
 import { logVerbose } from "../../globals.js";
 import { createInternalHookEvent, triggerInternalHook } from "../../hooks/hooks.js";
+import { matchMessageHandler } from "../../hooks/message-handler-match.js";
+import { runMessageHandler } from "../../hooks/message-handler-run.js";
+import type { MessageReceivedHookContext } from "../../hooks/internal-hooks.js";
 import { isDiagnosticsEnabled } from "../../infra/diagnostic-events.js";
 import {
   logMessageProcessed,
@@ -70,6 +74,10 @@ const resolveSessionTtsAuto = (
 export type DispatchFromConfigResult = {
   queuedFinal: boolean;
   counts: Record<ReplyDispatchKind, number>;
+  /** Set when a message handler took over processing */
+  handledByMessageHandler?: {
+    handlerId: string;
+  };
 };
 
 export async function dispatchReplyFromConfig(params: {
@@ -227,6 +235,64 @@ export async function dispatchReplyFromConfig(params: {
         },
       }),
     );
+
+    // Check for config-driven message handlers
+    const messageHandlers = cfg.hooks?.internal?.messageHandlers ?? [];
+    if (messageHandlers.length > 0) {
+      const handlerContext: MessageReceivedHookContext = {
+        from: ctx.From ?? "",
+        content,
+        timestamp,
+        channelId,
+        accountId: ctx.AccountId,
+        conversationId,
+        metadata: {
+          to: ctx.To,
+          provider: ctx.Provider,
+          surface: ctx.Surface,
+          threadId: ctx.MessageThreadId != null ? String(ctx.MessageThreadId) : undefined,
+          originatingChannel: ctx.OriginatingChannel,
+          originatingTo: ctx.OriginatingTo,
+          messageId: messageIdForHook,
+          senderId: ctx.SenderId,
+          senderName: ctx.SenderName,
+          senderUsername: ctx.SenderUsername,
+          senderE164: ctx.SenderE164,
+        },
+      };
+
+      const matchedHandler = matchMessageHandler(messageHandlers, handlerContext);
+      if (matchedHandler) {
+        const priority = matchedHandler.priority ?? "immediate";
+        const mode = matchedHandler.mode ?? "exclusive";
+
+        if (priority === "immediate") {
+          // Fire-and-forget for immediate handlers
+          const deps = createDefaultDeps();
+          void runMessageHandler({
+            cfg,
+            deps,
+            handler: matchedHandler,
+            context: handlerContext,
+          }).catch((err) => {
+            logVerbose(
+              `dispatch-from-config: message handler "${matchedHandler.id}" failed: ${String(err)}`,
+            );
+          });
+
+          // Check mode: exclusive (default) vs parallel
+          if (mode === "exclusive") {
+            // Return early - handler takes over completely
+            return {
+              queuedFinal: false,
+              counts: dispatcher.getQueuedCounts(),
+              handledByMessageHandler: { handlerId: matchedHandler.id },
+            };
+          }
+          // parallel mode: continue with normal flow
+        }
+      }
+    }
   }
 
   // Check if we should route replies to originating channel instead of dispatcher.

stevengonsalvez/clawdbot

src/auto-reply/reply/dispatch-from-config.ts
@@ -1,11 +1,12 @@
-import type { OpenClawConfig } from "../../config/config.js";
-import type { FinalizedMsgContext } from "../templating.js";
-import type { GetReplyOptions, ReplyPayload } from "../types.js";
-import type { ReplyDispatcher, ReplyDispatchKind } from "./reply-dispatcher.js";
 import { resolveSessionAgentId } from "../../agents/agent-scope.js";
+import { createDefaultDeps } from "../../cli/deps.js";
+import type { OpenClawConfig } from "../../config/config.js";
 import { loadSessionStore, resolveStorePath } from "../../config/sessions.js";
 import { logVerbose } from "../../globals.js";
 import { createInternalHookEvent, triggerInternalHook } from "../../hooks/hooks.js";
+import { matchMessageHandler } from "../../hooks/message-handler-match.js";
+import { runMessageHandler } from "../../hooks/message-handler-run.js";
+import type { MessageReceivedHookContext } from "../../hooks/internal-hooks.js";
 import { isDiagnosticsEnabled } from "../../infra/diagnostic-events.js";
 import {
   logMessageProcessed,
@@ -15,8 +16,11 @@ import {
 import { getGlobalHookRunner } from "../../plugins/hook-runner-global.js";
 import { maybeApplyTtsToPayload, normalizeTtsAutoMode, resolveTtsConfig } from "../../tts/tts.js";
 import { getReplyFromConfig } from "../reply.js";
+import type { FinalizedMsgContext } from "../templating.js";
+import type { GetReplyOptions, ReplyPayload } from "../types.js";
 import { formatAbortReplyText, tryFastAbortFromMessage } from "./abort.js";
 import { shouldSkipDuplicateInbound } from "./inbound-dedupe.js";
+import type { ReplyDispatcher, ReplyDispatchKind } from "./reply-dispatcher.js";
 import { isRoutableChannel, routeReply } from "./route-reply.js";
 
 const AUDIO_PLACEHOLDER_RE = /^<media:audio>(\s*\([^)]*\))?$/i;
@@ -78,6 +82,10 @@ const resolveSessionTtsAuto = (
 export type DispatchFromConfigResult = {
   queuedFinal: boolean;
   counts: Record<ReplyDispatchKind, number>;
+  /** Set when a message handler took over processing */
+  handledByMessageHandler?: {
+    handlerId: string;
+  };
 };
 
 export async function dispatchReplyFromConfig(params: {
@@ -241,6 +249,64 @@ export async function dispatchReplyFromConfig(params: {
         },
       }),
     );
+
+    // Check for config-driven message handlers
+    const messageHandlers = cfg.hooks?.internal?.messageHandlers ?? [];
+    if (messageHandlers.length > 0) {
+      const handlerContext: MessageReceivedHookContext = {
+        from: ctx.From ?? "",
+        content,
+        timestamp,
+        channelId,
+        accountId: ctx.AccountId,
+        conversationId,
+        metadata: {
+          to: ctx.To,
+          provider: ctx.Provider,
+          surface: ctx.Surface,
+          threadId: ctx.MessageThreadId != null ? String(ctx.MessageThreadId) : undefined,
+          originatingChannel: ctx.OriginatingChannel,
+          originatingTo: ctx.OriginatingTo,
+          messageId: messageIdForHook,
+          senderId: ctx.SenderId,
+          senderName: ctx.SenderName,
+          senderUsername: ctx.SenderUsername,
+          senderE164: ctx.SenderE164,
+        },
+      };
+
+      const matchedHandler = matchMessageHandler(messageHandlers, handlerContext);
+      if (matchedHandler) {
+        const priority = matchedHandler.priority ?? "immediate";
+        const mode = matchedHandler.mode ?? "exclusive";
+
+        if (priority === "immediate") {
+          // Fire-and-forget for immediate handlers
+          const deps = createDefaultDeps();
+          void runMessageHandler({
+            cfg,
+            deps,
+            handler: matchedHandler,
+            context: handlerContext,
+          }).catch((err) => {
+            logVerbose(
+              `dispatch-from-config: message handler "${matchedHandler.id}" failed: ${String(err)}`,
+            );
+          });
+
+          // Check mode: exclusive (default) vs parallel
+          if (mode === "exclusive") {
+            // Return early - handler takes over completely
+            return {
+              queuedFinal: false,
+              counts: dispatcher.getQueuedCounts(),
+              handledByMessageHandler: { handlerId: matchedHandler.id },
+            };
+          }
+          // parallel mode: continue with normal flow
+        }
+      }
+    }
   }
 
   // Check if we should route replies to originating channel instead of dispatcher.